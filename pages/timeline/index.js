9796215e324670722a94bd07b7c4539b216d778e
aitorevi <aitorevi@gmail.com>
2022-10-03 00:10:54 +0200

    first commit

---
+++ pages/timeline/index.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import AppLayout from "../../components/AppLayout";
+
+export default function Timeline({ userName }) {
+  return (
+    <>
+      <AppLayout>
+        <h1>Timeline de {userName}</h1>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </AppLayout>
+      <style jsx>{`
+        h1 {
+          font-size: 36px;
+          color: red;
+        }
+      `}</style>
+    </>
+  );
+}
+
+Timeline.getInitialProps = () => {
+  return fetch("http://localhost:3000/api/hello")
+    .then((res) => res.json())
+    .then((response) => {
+      console.log(response);
+      return response;
+    });
+};
